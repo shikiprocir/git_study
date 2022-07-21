7a1af0d38ff2e1751cc525346484638f3f1b1613
shikiprocir <shikikensuke@gmail.com>
2022-07-21 22:42:08 +0900

    サブ課題11追記

--- link.php
+++ link.php
@@ -2,10 +2,10 @@
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>課題20リンク集</title>
+<title>課題20リンク集（サブ課題11）</title>
 </head>
 <body>
-<h1>課題20リンク集</h1>
+<h1>課題20リンク集（サブ課題11）</h1>
 <a href="https://procir-study.site/shiki402/maintask/task20/git_study/character_count.php">文字数カウント</a><br>
 <br>
 <a href="https://procir-study.site/shiki402/maintask/task20/git_study/current_date_time.php">現在日時</a><br>
